ced54795ed8c13285b8a7de482478c7f1021215f
Nikhil Bhamidipati <79391745+bhamidipatinikhil@users.noreply.github.com>
2022-07-20 01:15:09 -0700

    Update Program4.java
    Put the Reversing String block in a function to make the program look neater!

--- Itellij-workspace/Data Structures Through Java Lab Programs/Program4.java
+++ Itellij-workspace/Data Structures Through Java Lab Programs/Program4.java
@@ -1,23 +1,31 @@
 import java.util.*;
+
 public class Program4 {
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.print("Enter String : ");
-        String p = s.next();
-        String r = "";
-        for(int i=p.length()-1;i>=0;i--)
-        {
-        	r+=p.charAt(i);
-        }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter String : ");
+
+        String p = s.nextLine();
+        String r = reverseString(p);
+
         if(p.equals(r))
         {
-        System.out.print(r+" is Palindrome");
-	    }
+            System.out.print("\"" + r+"\" is Palindrome");
+        }
         else
         {
-        System.out.print(p+" is not a Palindrome");
+            System.out.print("\"" + r+"\" is not a Palindrome");
         }
-	}
+    }
+
+    public static String reverseString(String p){
+        String r = "";
+        for(int i=p.length()-1; i>=0; i--)
+        {
+            r+=p.charAt(i);
+        }
+        return r;
+    }
 
 }
