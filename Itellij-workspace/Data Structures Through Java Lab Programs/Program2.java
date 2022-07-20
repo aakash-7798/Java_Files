4f839a12370786c7f61b6bf11aab3ca9433b07d4
Nikhil Bhamidipati <79391745+bhamidipatinikhil@users.noreply.github.com>
2022-07-20 00:48:21 -0700

    Update Program2.java

--- Itellij-workspace/Data Structures Through Java Lab Programs/Program2.java
+++ Itellij-workspace/Data Structures Through Java Lab Programs/Program2.java
@@ -4,35 +4,63 @@
 //the nth value in the Fibonacci sequence.
 
 import java.util.*;
+
+
 public class Program2 {
-	
-	public static int Fibinocci(int n)
-	{
-		if(n>=0 && n<=1)
-			return n;
-		else
-			return Fibinocci(n-1)+Fibinocci(n-2);
-	}
-	
-	public static void main(String[] args) {
-          Scanner s = new Scanner(System.in);
-          System.out.print("Enter a number to which you want fibinooci sequence : ");
-          int fib = s.nextInt();
-          int a[] = new int[fib];
-          a[0] = 1;
-          a[1] = 1;
-          System.out.print("The Fibinocci Sequence Without Recursion : "+a[0]+" "+a[1]);
-          for(int i=2;i<a.length;i++)
-          {
-        	  a[i]=a[i-1]+a[i-2];
-        	  System.out.print(" "+a[i]+" ");
-          }
-          System.out.println(" ");
-          System.out.print("The Fibinocci Sequence With Recursion    :");
-          for(int i=1;i<=fib;i++)
-          {
-          System.out.print(" "+Fibinocci(i)+" ");
-          }
-	}
-	
+
+    public static int Fibinocci(int n)
+    {
+
+        if(n==1 || n==2)
+            return 1;
+        else
+            return Fibinocci(n-1)+Fibinocci(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("I will give the first n Fibonacci Numbers!\nEnter n:: ");
+        int n = s.nextInt();
+
+        try {
+            if(n <= 0){
+                throw new Exception("n should be greater than 0");
+            }
+
+            int a[] = new int[n];
+            System.out.print("All elements of the initial array are initialised to 0. a:: ");
+            System.out.println(Arrays.toString(a));
+            /*
+            The 1st Fibonacci:: a[0]
+            The nth fibonacci:: a[n-1]
+             */
+
+            // The Non Recursive case fails for n==1, unless we return for n==1 directly
+            if(n==1){
+                System.out.println("The Fibonacci Sequence Without Recursion : " + 1);
+            }
+            else{
+                a[0] = 1;
+                a[1] = 1;
+                System.out.print("The Fibinocci Sequence Without Recursion : " + a[0] + "  " + a[1] + " ");
+                for (int i = 2; i < n; i++) {
+                    a[i] = a[i - 1] + a[i - 2];
+                    System.out.print(" " + a[i] + " ");
+                }
+                System.out.println(" ");
+            }
+
+
+            // Recursive Implementation
+            System.out.print("The Fibinocci Sequence With Recursion    :");
+            for (int i = 1; i <= n; i++) {
+                System.out.print(" " + Fibinocci(i) + " ");
+            }
+        }
+        catch(Exception e){
+            System.out.print("Exception caught: ");
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
